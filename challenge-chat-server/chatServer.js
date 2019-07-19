b774793041bab95e4bbb318254b919a8844e19a9
Coder <ethiogentelman.com>
2019-07-20 00:40:15 +0100

    load the 'express' module and add simple route

---
+++ challenge-chat-server/chatServer.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+
+app.get("/", (req,res)=>{
+    res.send("hello chat!")
+})
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is listening on port 3000. Ready to accept requests!");
+  });
+  
