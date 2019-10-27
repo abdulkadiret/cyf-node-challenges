76fdf4be815fa7908b1d2b4b4f9ab62dd16ffea4
abdulkadiret <ethiogentelman@gmail.com>
2019-10-27 12:58:09 +0000

    Create a config.js file

---
+++ challenge-chat-server/config.js
@@ -0,0 +1,16 @@
+const config = {
+  development: {
+    user: "akey",
+    host: "localhost",
+    database: "chat_app",
+    password: "password",
+    port: 5432
+  },
+  production: {
+    connectionString: process.env.DATABASE_URL
+  }
+};
+
+const env = process.env.NODE_ENV || "development";
+
+module.exports = config[env];
