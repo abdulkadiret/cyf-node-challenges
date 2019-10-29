38d50f6c3876fb5bcd56c85085dd3c48f3fe86ec
abdulkadiret <ethiogentelman@gmail.com>
2019-10-29 03:31:22 +0000

    Create messages endpoints / Routes

---
+++ challenge-chat-server/api/messages.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllMessages,
+  getMessageByUserId,
+  createMessage,
+  deleteMessage
+} = require("../services/database/messages");
+
+/**
+ * The route here will be: /users/ (remember the prefix users is defined in api/index.js)
+ */
+router.get("/", (req, res) => {
+  getAllMessages()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+router.get("/:userId", (req, res) => {
+  const { userId } = req.params;
+  getMessageByUserId(userId)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+router.post("/", (req, res) => {
+  const { message, userId } = req.body;
+  createMessage({ message, userId })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+router.delete("/:messageId", (req, res) => {
+  const { messageId } = req.params;
+  deleteMessage(messageId)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+module.exports = router;
