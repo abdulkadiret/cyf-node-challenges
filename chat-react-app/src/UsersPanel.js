6bbf93f3b0b5286841bc342e8984307eb1ab957d
abdulkadiret <ethiogentelman@gmail.com>
2019-10-30 23:52:30 +0000

    Create getUsers function to handle signed-in users

--- chat-react-app/src/UsersPanel.js
+++ chat-react-app/src/UsersPanel.js
@@ -1,62 +1,142 @@
 import React, { Component } from "react";
 import "./UsersPanel.css";
 
-// class UsersPanel extends Component {
-//   render() {
-//     if (this.props.users) {
-//       return (
-//         <ul>
-//           {this.props.users.map((user, index) => {
-//             return <li>{user}</li>;
-//           })}
-//         </ul>
-//       );
-//     } else {
-//       return <p>Loading... </p>;
-//     }
-//   }
-// }
-
-// export default UsersPanel;
-
 class UsersPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+    setInterval(this.getUsers, 500);
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    const url = "http://localhost:3002/api/users/signed-in";
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        const signedInUserId = localStorage.getItem("signedInUserId");
+        const signedUser =
+          data &&
+          data.filter(user => {
+            return user.signed_in_user_id === JSON.parse(signedInUserId);
+          });
+        if (!signedUser || !signedUser.length) {
+          localStorage.setItem("signedInUserId", "");
+          window.location.href = "/login";
+        }
+        this.setState({ users: data });
+      });
+  };
+
   render() {
-    return (
-      <div>
-        <h4 className="us-dis">Users Display</h4>
-        <ul className="user-pane">
-          <li>Peter</li>
-          <li>Akeygggggggggggggggggggssssssssssssssssss</li>
-          <li>Mimi</li>
-          <li>Peter</li>
-          <li>Akey</li>
-          <li>Mimi</li>
-          <li>Peter</li>
-          <li>Akey</li>
-          <li>Mimi</li>
-          <li>Peter</li>
-          <li>Akey</li>
-          <li>Mimi</li>
-          <li>Peter</li>
-          <li>Akey</li>
-          <li>Mimi</li>
-          <li>Peter</li>
-          <li>Akey</li>
-          <li>Mimi</li>
-          <li>Peter</li>
-          <li>Akey</li>
-          <li>Mimi</li>
-          <li>Peter</li>
-          <li>Peter</li>
-          <li>Akey</li>
-          <li>Mimi</li>
-          <li>Peter</li>
-          <li>Akey</li>
-          <li>Mimi</li>
+    const { users } = this.state;
+    if (users) {
+      return (
+        <ul>
+          {users.map((user, index) => {
+            return <li key={index}>{user.name}</li>;
+          })}
         </ul>
-      </div>
-    );
+      );
+    } else {
+      return <p>Loading... </p>;
+    }
   }
 }
 
 export default UsersPanel;
+
+// class UsersPanel extends Component {
+//   render() {
+//     return (
+//       <div>
+//         {/* <h4 className="users-display">Users Display</h4> */}
+//         <ul>
+//           <li>Peter</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default UsersPanel;// class UsersPanel extends Component {
+//   render() {
+//     return (
+//       <div>
+//         {/* <h4 className="users-display">Users Display</h4> */}
+//         <ul>
+//           <li>Peter</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//           <li>Peter</li>
+//           <li>Akey</li>
+//           <li>Mimi</li>
+//           <li>Peter</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default UsersPanel;
