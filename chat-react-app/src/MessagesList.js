ec57b32e51b46d97c692eef0d267300c7778c21a
abdulkadiret <ethiogentelman@gmail.com>
2019-10-30 23:18:38 +0000

    Change the url to back end api messages route

--- chat-react-app/src/MessagesList.js
+++ chat-react-app/src/MessagesList.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import Input from "./Input";
 import "./MessagesList.css";
 
 class MessagesList extends Component {
@@ -10,25 +9,28 @@ class MessagesList extends Component {
       doScroll: true
     };
     this.messagesEnd = React.createRef();
-    setInterval(this.loadMessages, 500);
+    setInterval(this.onLoadMessages, 500);
   }
 
-  onSentMessage = () => {
-    this.loadMessages();
+  handleOnSentMessage = () => {
+    this.onLoadMessages();
   };
 
-  loadMessages = () => {
-    const url = "http://localhost:3002/messages";
+  onLoadMessages = () => {
+    const url = "http://localhost:3002/api/messages";
     fetch(url)
       .then(res => res.json())
       .then(data => {
-        let newMessageAdded = this.state.messages.length !== data.length;
-        this.setState({ messages: data, doScroll: newMessageAdded });
+        let onNewMessageAdd = this.state.messages.length !== data.length;
+        data = data.sort((a, b) => {
+          return a.message_id - b.message_id;
+        });
+        this.setState({ messages: data, doScroll: onNewMessageAdd });
       });
   };
 
   componentDidMount() {
-    this.loadMessages();
+    this.onLoadMessages();
   }
 
   componentDidUpdate() {
@@ -38,38 +40,12 @@ class MessagesList extends Component {
     }
   }
 
-  deleteMessage = e => {
-    // this method on the event stops any other onClick
-    // handlers being called - we only want the button
-    // onClick to be called, not the one on the parent
-    // paragraph
-    e.stopPropagation();
-    // we split this into an array like this:
-    // ["delete", 5]
-    // then take the second element with [1]
-    let deleteId = e.target.id.split("-")[1];
-    console.log("delete id was", e.target.id, "but we made it into", deleteId);
-    // let deleteMessage = Chats.find(chat => chat.id === id);
-    // Chats.splice(Chats.indexOf(deleteChat), 1);
-    // res.send(Chats);
-
-    // let messages = this.state.messages.splice(deleteId, 1);
-    // this.setState({ messages });
-    const url = `http://localhost:3002/messages/${deleteId}`;
+  handleDeleteMessage = messageId => {
+    const url = `http://localhost:3002/api/messages/${messageId}`;
     fetch(url, {
       method: "DELETE"
     });
-    this.loadMessages();
-    // .then(res => res.json()) // OR res.text()
-    // .then(res => console.log(res));
-    // .then(data => {
-    //   console.log("Success:", JSON.stringify(data));
-    //   this.onSentMessage();
-    //   this.setState({
-    //     messages: messages
-    //   });
-    // })
-    // .catch(error => console.log("Error", "input a message"));
+    this.onLoadMessages();
   };
 
   editMessage = e => {
@@ -83,36 +59,37 @@ class MessagesList extends Component {
     });
   };
 
-  updateMessage = e => {
-    let editedId = e.target.id.split("-")[1];
-    console.log("edit id was", e.target.id, "but we made it into", editedId);
-    const url = `http://localhost:3002/messages`;
-    const oldMessage = this.state.messages.find(elem => elem.id === editedId);
+  handleUpdateMessage = (e, messageId) => {
+    e.stopPropagation();
+    // let editedId = e.target.id.split("-")[1];
+    // console.log("edit id was", e.target.id, "but we made it into", editedId);
+    const url = `http://localhost:3002/api/messages/${messageId}`;
+    // const oldMessage = this.state.messages.find(elem => elem.id === editedId);
+    const userId = localStorage.getItem("userId");
     let newMessage = {
-      id: oldMessage.id,
-      from: oldMessage.from,
-      text: e.target.value
+      userId,
+      message: e.target.value
     };
 
     fetch(url, {
-      method: "put",
+      method: "PUT",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(newMessage)
     });
-    this.loadMessages();
+    this.onLoadMessages();
   };
 
   handleClick = e => {
     let messages = this.state.messages;
-    // if a message is being edited, we don't allow any other messages
+    // if a message is being edited, don't allow any other messages
     // to be edited or deleted until it's finished
     if (messages.some(m => m.inEditMode)) return;
-    let offset = messages.findIndex(elem => elem.id === e.target.id);
-    let showButtons = !messages[offset].showButtons;
-    messages.forEach(message => (message.showButtons = false));
-    messages[offset].showButtons = showButtons;
+    // let offset = messages.findIndex(elem => elem.id === e.target.id);
+    // let showButtons = !messages[offset].showButtons;
+    // messages.forEach(message => (message.showButtons = false));
+    // messages[offset].showButtons = showButtons;
     this.setState({
       messages: messages
     });
@@ -121,7 +98,7 @@ class MessagesList extends Component {
   editKeyDown = e => {
     // Update the message only when we hit enter.
     if (e.keyCode === 13) {
-      this.updateMessage(e);
+      this.handleUpdateMessage(e);
     }
   };
 
@@ -136,49 +113,65 @@ class MessagesList extends Component {
   }
 
   render() {
+    const userId = localStorage.getItem("userId");
     return (
-      <div className="message-board">
+      <div>
         <ul className="messages">
-          {this.state.messages.map((message, index) => (
-            <li key={message.id}>
-              <h4>{message.from}</h4>
-              <p onClick={this.handleClick} className="card" id={message.id}>
-                {message.inEditMode ? (
-                  <input
-                    autoFocus
-                    type="text"
-                    defaultValue={message.text}
-                    style={{ width: "100%" }}
-                    onClick={e => e.stopPropagation()}
-                    onKeyDown={this.editKeyDown}
-                    id={"input-" + message.id}
-                  />
-                ) : (
-                  message.text
-                )}
-                {!message.inEditMode && message.showButtons ? (
-                  <span className="butn">
-                    <button
-                      className="btn btn-danger"
-                      onClick={this.deleteMessage}
-                      id={"delete-" + message.id}
-                    >
-                      Delete
-                    </button>
-                    <button
-                      className="btn btn-primary"
-                      onClick={this.editMessage}
-                      id={"edit-" + message.id}
-                    >
-                      Edit
-                    </button>
-                  </span>
-                ) : (
-                  ""
-                )}
-              </p>
-            </li>
-          ))}
+          {this.state.messages.map((message, index) => {
+            return (
+              <li key={index}>
+                <div className="sender">{message.name}</div>
+                <p
+                  // onClick={this.handleClick}
+                  className="bubble"
+                  // id={message.id}
+                >
+                  {message.message}
+                  {/* {message.inEditMode ? (
+                    <input
+                      autoFocus
+                      type="text"
+                      defaultValue={message.text}
+                      style={{ width: "100%" }}
+                      onClick={e => e.stopPropagation()}
+                      onKeyDown={this.editKeyDown}
+                      id={"input-" + message.id}
+                    />
+                  ) : (
+                  
+                  )} */}
+
+                  {JSON.parse(userId) === message.user_id ? (
+                    <span style={{ display: "block" }}>
+                      <button
+                        className="link-button"
+                        style={{ display: "inline-block", float: "right" }}
+                        onClick={() =>
+                          window.confirm(
+                            "Are you sure you wish to delete this message?"
+                          ) && this.handleDeleteMessage(message.message_id)
+                        }
+                      >
+                        <i className="fa fa-trash fa-lg"></i>
+                      </button>
+                      <button
+                        className="link-button"
+                        style={{
+                          display: "inline-block",
+                          float: "right",
+                          marginRight: "10px"
+                        }}
+                        onClick={this.editMessage}
+                        id={"edit-" + message.id}
+                      >
+                        <i className="fa fa-pencil-square-o fa-lg"></i>
+                      </button>
+                    </span>
+                  ) : null}
+                </p>
+              </li>
+            );
+          })}
         </ul>
         <div ref={this.messagesEnd}></div>
       </div>
