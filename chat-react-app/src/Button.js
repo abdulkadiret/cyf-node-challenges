cff4d40d47f17ebec729ae0839781651de8ac2c1
abdulkadiret <ethiogentelman@gmail.com>
2019-10-30 00:17:01 +0000

    Add type attribute  for button

--- chat-react-app/src/Button.js
+++ chat-react-app/src/Button.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Button() {
-  return <button {...props} className="sendBtn" />;
+function Button(props) {
+  return (
+    <button type="submit" className="btn btn-secondary m-2">
+      {props.name}
+    </button>
+  );
 }
 
 export default Button;
