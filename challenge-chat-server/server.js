e2ec557755cbd33075118826b26eb140a7cb20ea
abdulkadiret <ethiogentelman@gmail.com>
2019-11-24 20:21:53 +0000

    Add endpoint to be used in production environment

--- challenge-chat-server/server.js
+++ challenge-chat-server/server.js
@@ -12,6 +12,16 @@ app.use(cors());
 app.use("/api/users", users);
 app.use("/api/messages", messages);
 
+if (process.env.NODE_ENV === production) {
+  app.use(express.static("chat-react-app/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "chat-react-app", "build", "index.html")
+  );
+});
+
 app.listen(process.env.PORT || 3002, function() {
   console.log("Server is listening on port 3002. Ready to accept requests!");
 });
