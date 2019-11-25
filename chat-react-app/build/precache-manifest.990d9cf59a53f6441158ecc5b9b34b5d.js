9f8dd80d9cfe83b33bd4750822d6f1a7f5c319bc
abdulkadiret <ethiogentelman@gmail.com>
2019-11-25 16:07:46 +0000

    Create production build

---
+++ chat-react-app/build/precache-manifest.990d9cf59a53f6441158ecc5b9b34b5d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c361682479c7d00f0ec4ad9dffb2d14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "669ef73595e8190d6d8f",
+    "url": "/static/css/2.5dd60c9c.chunk.css"
+  },
+  {
+    "revision": "5d710af76c4c5b577cf9",
+    "url": "/static/css/main.26fcdea0.chunk.css"
+  },
+  {
+    "revision": "669ef73595e8190d6d8f",
+    "url": "/static/js/2.0c18d8d5.chunk.js"
+  },
+  {
+    "revision": "5d710af76c4c5b577cf9",
+    "url": "/static/js/main.e932c2c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
