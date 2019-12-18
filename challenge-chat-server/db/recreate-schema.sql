f0336999716356875bbc12a4e0f118bb49ce07dd
abdulkadiret <ethiogentelman@gmail.com>
2019-12-18 19:10:29 +0000

    Add recreate-db schema to start script & reorder schema tables

--- challenge-chat-server/db/recreate-schema.sql
+++ challenge-chat-server/db/recreate-schema.sql
@@ -3,24 +3,27 @@
 
 DROP TABLE IF EXISTS messages;
 
-DROP TABLE IF EXISTS users;
-
 DROP TABLE IF EXISTS signed_in_users;
 
-CREATE TABLE users (
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
   user_id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   email VARCHAR(40) NOT NULL,
   password VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE messages (
+CREATE TABLE messages
+(
   message_id SERIAL PRIMARY KEY,
   message TEXT DEFAULT NULL,
   user_id INTEGER REFERENCES users (user_id)
 );
 
-CREATE TABLE signed_in_users (
+CREATE TABLE signed_in_users
+(
   signed_in_user_id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users (user_id),
   name VARCHAR(100) DEFAULT NULL
